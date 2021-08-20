33b6f055185a986bbc7415ec6aa02d42db2e8156
M1072324 <thiru@Mindtree>
2021-08-20 16:39:32 +0530

    Create The ItemController changes

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(List<Item> itemlist)
+        {
+            //itemlist = _context.Items.ToList();
+            //return View("Index",itemlist);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            var items = _context.Items.Find(Id);
+            _context.Items.Remove(items);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
